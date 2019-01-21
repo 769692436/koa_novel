6008d7a2215006607ef5df182fa7d2fa71e01715
KIng_Kel <769692436@qq.com>
2019-01-21 17:12:39 +0800

    添加小说

--- public/js/book_add.js
+++ public/js/book_add.js
@@ -3,4 +3,10 @@ layui.use(['form', 'upload'], function(){
   var upload = layui.upload;
   var $ = layui.$;
   var layer = layui.layer;
+
+
+  var uploadInst = upload.render({
+    elem: '#upload-cover',
+    url: '/admin/book/add'
+  });
 });
